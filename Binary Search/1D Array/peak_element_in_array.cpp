b5c70ccd1a7a16322626c80496a41e569413a805
ShauryaaSharma <shauryasofficial27@gmail.com>
2026-06-21 16:46:12 +0530

    Mq

--- Binary Search/1D Array/peak_element_in_array.cpp
+++ Binary Search/1D Array/peak_element_in_array.cpp
@@ -3,41 +3,23 @@ using namespace std;
 
 class Solution{
     public:
-    int bruteForce(int *A, int n){
-
-        if (n == 1) return A[0];
-
-        for(int i = 0; i < n; i ++){
-            if(i == 0 && A[i] != A[i+1]){
-                return A[i];
-            }else if(i == n-1 && A[i] != A[i-1]){
-                return A[i];
-            }else if(A[i] != A[i-1] && A[i] != A[i+1]){
-                return A[i];
-            }
-        }
-        return -1;
-    }
-
     int optimalApproch(int *A, int n){
+        if(n == 1){
+            return 0;
+        }else if(A[0] > A[1]){
+            return A[0];
+        }else if(A[n-1] > A[n-2]){
+            return A[n-1];
+        }
+
         int low = 1;
         int high = n-2;
-
-        if(n == 1){
-            return A[0];
-        }else if (A[0] != A[1]){
-            return A[0];
-        }
-        else if (A[n - 1] != A[n - 2]){
-            return A[n - 1];
-        }
-
         while(low <= high){
             int mid = low + (high - low) / 2;
-            cout << "The value of the middle is: " << mid << endl;
-            if(A[mid] != A[mid-1] && A[mid] != A[mid+1]){
+
+            if(A[mid] > A[mid - 1] && A[mid] > A[mid + 1]){
                 return A[mid];
-            }else if((A[mid] == A[mid - 1] && mid % 2 == 1) || (A[mid] == A[mid + 1] && mid % 2 == 0) ){
+            }else if(A[mid] > A[mid - 1]){
                 low = mid + 1;
             }else {
                 high = mid - 1;
